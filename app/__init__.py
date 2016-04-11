62fc12d4f91e48dad45a1d5642b6f177c71b488f
Preetam Shingavi <preetams3@gmail.com>
2016-04-11 01:04:34 -0700

    Twitter bootstrap integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from config import config
+from flask_bootstrap import Bootstrap
+
+# Instantiate extentions
+bootstrap = Bootstrap()
+
 
 def create_app(config_name):
     app = Flask(__name__)
 
     # Add config to the flask app
     app.config.from_object(config[config_name])
+
+    # Initialize with bootstrap
+    bootstrap.init_app(app)
 
     # Register all the blueprints
     from .talks import talks as talk_blueprint
