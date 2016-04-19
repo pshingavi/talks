feb85bb24ed7121981513293408b8155151ec00f
Preetam Shingavi <preetams3@gmail.com>
2016-04-18 18:33:51 -0700

    Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from config import config
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 # Instantiate extentions
 bootstrap = Bootstrap()
-
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -14,6 +15,9 @@ def create_app(config_name):
 
     # Initialize with bootstrap
     bootstrap.init_app(app)
+
+    # Initialize with SQLAlchemy
+    db.init_app(app)
 
     # Register all the blueprints
     from .talks import talks as talk_blueprint
--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -8,13 +9,18 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 't0p s3cr3t'
-
+    DB_URI = os.environ.get('DEV_DB_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 class TestingConfig(Config):
     TESTING = True
+    DB_URI = os.environ.get('TEST_DB_URI') or \
+             'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 
 class ProductionConfig(Config):
+    DB_URI = os.environ.get('PROD_DB_URI') or \
+             'sqlite:///' + os.path.join(basedir, 'prod-dev.sqlite')
     pass
 
 
