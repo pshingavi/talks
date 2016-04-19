72fa275ed09ee0556ddd59230aa7c0b718eac3c8
Preetam Shingavi <preetams3@gmail.com>
2016-04-18 18:52:43 -0700

    Authentication blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -23,4 +23,7 @@ def create_app(config_name):
     from .talks import talks as talk_blueprint
     app.register_blueprint(talk_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
     return app
